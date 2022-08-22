f94e7cc875d54276bb9e7ff4853df60fc2799f26
AntonBosov <106320087+AntonBosov@users.noreply.github.com>
2022-08-22 11:38:39 +0300

    Update Points.java

--- src/test/java/ru/stqa/pft/sandbox/Points.java
+++ src/test/java/ru/stqa/pft/sandbox/Points.java
@@ -9,9 +9,11 @@ public class Points{
   public static void main(String[] args) {
     Point p1 = new Point(0, 1);
     Point p2 = new Point(2, -2);
+    System.out.println("points distance2 =" + distance2(p1, p2));
     System.out.println("points distance =" + distance(p1, p2));
+    Assert.assertEquals(distance2(p1, p2), distance(p1, p2));
   }
-  //public static double distance (Point p1,Point p2){
-   // return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y));
-  //}
+  public static double distance (Point p1,Point p2){
+    return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y));
+  }
 }
