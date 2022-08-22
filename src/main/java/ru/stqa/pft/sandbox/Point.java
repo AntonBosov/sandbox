bc530103f684c7f7434342a196a19e73a65fbf23
AntonBosov <106320087+AntonBosov@users.noreply.github.com>
2022-08-22 10:56:56 +0300

    Update Point.java

--- src/main/java/ru/stqa/pft/sandbox/Point.java
+++ src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,15 +1,15 @@
 package ru.stqa.pft.sandbox;
 
 public class Point {
-  public double x;
-  public double y;
+  private double x;
+  private double y;
   public Point (double x, double y) {
     this.x=x;
     this.y=y;
 
   }
-  public double getX(){return x;}
-  public double getY(){return y;}
+  private double getX(){return x;}
+  private double getY(){return y;}
   public static double distance (Point p1,Point p2){
     return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y));
   }
