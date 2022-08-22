17de03a7d6acf404a018206ab86b378b6e387f37
AntonBosov <106320087+AntonBosov@users.noreply.github.com>
2022-08-22 10:52:10 +0300

    Add files via upload

--- src/test/java/ru/stqa/pft/sandbox/Points.java
+++ src/test/java/ru/stqa/pft/sandbox/Points.java
@@ -1,13 +1,17 @@
 package ru.stqa.pft.sandbox;
 
+import java.awt.*;
+
+import static ru.stqa.pft.sandbox.Point.distance;
+
 public class Points{
 
   public static void main(String[] args) {
-
-    Point P = new Point(0, 1, 2, -2);
-    System.out.println("points distance =" + distance(P));
+    Point p1 = new Point(0, 1);
+    Point p2 = new Point(2, -2);
+    System.out.println("points distance =" + distance(p1, p2));
   }
-  public static double distance (Point P){
-    return Math.sqrt((P.p3-P.p1)*(P.p3-P.p1)+(P.p4-P.p2)*(P.p4-P.p2));
-  }
+  //public static double distance (Point p1,Point p2){
+   // return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y));
+  //}
 }
